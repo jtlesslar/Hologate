5e176416987fd4c4d404d8b7d607de51339b0487
James Lesslar <jlesslar@hotmail.co.uk>
2020-03-02 23:03:27 +0000

    Tidy up of PLayerPawn and add Keyboard Arrow Keys for Player 0 Rotation

--- Source/Hologate/PlayerPawn.cpp
+++ Source/Hologate/PlayerPawn.cpp
@@ -1,16 +1,27 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "PlayerPawn.h"
 #include "Projectile.h"
 
 #include "Camera/CameraComponent.h"
 #include "Components/StaticMeshComponent.h"
 #include "Components/InputComponent.h"
-
-#include <UObject/ConstructorHelpers.h>
+#include "Kismet/GameplayStatics.h"
 #include "PlayerMovementComponent.h"
-#include <Kismet/GameplayStatics.h>
+
+#include <TimerManager.h>
+#include <UObject/ConstructorHelpers.h>
+
+namespace 
+{
+	const float InputClampMin = -1.0f;
+	const float InputClampMax = 1.0f;
+	const float FireRateDefault = 0.1f;
+	const float RespawnRateDefault = 2.0f;
+
+	const int32 FullHealth = 100;
+	const int32 DamagePerShot = 20;
+
+}
 
 // Sets default values
 APlayerPawn::APlayerPawn()
@@ -18,13 +29,13 @@ APlayerPawn::APlayerPawn()
 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	// Our root component will be a sphere that reacts to physics
+	//Create a Sphere Component for Collision Detection
 	USphereComponent* SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("RootComponent"));
 	RootComponent = SphereComponent;
 	SphereComponent->InitSphereRadius(40.0f);
 	SphereComponent->SetCollisionProfileName(TEXT("Pawn"));
-
-	// Create and position a mesh component so we can see where our sphere is
+		
+	//Create a Sphere Mesh to Visualize the Player
 	UStaticMeshComponent* SphereVisual = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisualRepresentation"));
 	SphereVisual->SetupAttachment(RootComponent);
 	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereVisualAsset(TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere"));
@@ -36,12 +47,8 @@ APlayerPawn::APlayerPawn()
 	
 		PlayerMesh = SphereVisual;
 	}
-
-	// Players can't walk on it
-	SphereComponent->SetWalkableSlopeOverride(FWalkableSlopeOverride(WalkableSlope_Unwalkable, 0.f));
-	SphereComponent->CanCharacterStepUpOn = ECB_No;
 	
-
+	//Create a Cube Mesh to Visualize the Weapon
 	UStaticMeshComponent* WeaponVisual = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WeaponRepresentation"));
 	WeaponVisual->SetupAttachment(RootComponent);
 	static ConstructorHelpers::FObjectFinder<UStaticMesh> WaeponVisualAsset(TEXT("/Game/StarterContent/Shapes/Shape_Cube.Shape_Cube"));
@@ -54,14 +61,15 @@ APlayerPawn::APlayerPawn()
 		WeaponMesh = WeaponVisual;
 	}
 
-	FireRate = 0.1f;
-	bCanFire = true;
-	Health = 100;
-
 	// Create an instance of our movement component, and tell it to update the root.
 	OurMovementComponent = CreateDefaultSubobject<UPlayerMovementComponent>(TEXT("CustomMovementComponent"));
 	OurMovementComponent->UpdatedComponent = RootComponent;
-	   
+
+	//Set Defaults
+	FireRate = FireRateDefault;
+	RespawnRate = RespawnRateDefault;
+	bCanShoot = true;
+	Health = FullHealth;
 }
 
 // Called when the game starts or when spawned
@@ -69,8 +77,10 @@ void APlayerPawn::BeginPlay()
 {
 	Super::BeginPlay();
 
+	//Assign Player to Controller
 	UGameplayStatics::CreatePlayer((GetWorld()));
 	   
+	//Hold onto Starting Location and Rotation for Respawns
 	StartingLocation = GetActorLocation();
 	StartingRotation = GetActorRotation();
 	
@@ -80,71 +90,62 @@ void APlayerPawn::BeginPlay()
 void APlayerPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	// Handle movement based on our "MoveX" and "MoveY" axes
+	
+	if (OurMovementComponent && 
+		(OurMovementComponent->UpdatedComponent == RootComponent) &&
+		(!MovementDirection.IsZero()))
 	{
-
-		//if (OurMovementComponent && 
-		//	(OurMovementComponent->UpdatedComponent == RootComponent) &&
-		if	(!CurrentVelocity.IsZero())
-		{
-			OurMovementComponent->AddInputVector(CurrentVelocity* DeltaTime* 100.0f);
-		}
-
-		//if (!CurrentVelocity.IsZero())
-		//{
-		//	FVector NewLocation = GetActorLocation() + (CurrentVelocity * DeltaTime);
-		//	SetActorLocation(NewLocation);
-		//	
-		//	
-
-		//}
-
-		if (!RotationDirection.IsZero())
-		{
-			SetActorRotation(RotationDirection.Rotation());
-		}
+		OurMovementComponent->AddInputVector(MovementDirection);
 	}
 
+	if (!RotationDirection.IsZero())
 	{
-		if (bGrowing)
+		SetActorRotation(RotationDirection.Rotation());
+	}
+		
+	if (bShooting && bCanShoot)
+	{
+		if (Projectile != NULL)
 		{
-
-			if (bCanFire)
+			UWorld* World = GetWorld();
+						
+			if (World)
 			{
-
-				if (Projectile != NULL)
-				{
-					//todo getworld var
-					FVector MuzzleOffset = (GetActorForwardVector() * 90.0f) + FVector(0.0f, 0.0f, 50.0f);
-					GetWorld()->SpawnActor<AProjectile>(Projectile, GetActorLocation() + MuzzleOffset, GetActorRotation());
-					bCanFire = false;
-					GetWorld()->GetTimerManager().SetTimer(TimerHandle_ShotTimerExpired, this, &APlayerPawn::ShotTimerExpired, FireRate);
-				}
+				FVector MuzzleOffset = (GetActorForwardVector() * 90.0f) + FVector(0.0f, 0.0f, 50.0f);
+				World->SpawnActor<AProjectile>(Projectile, GetActorLocation() + MuzzleOffset, GetActorRotation());
+				bCanShoot = false;
+				World->GetTimerManager().SetTimer(TimerHandle_ShotTimerExpired, this, &APlayerPawn::ShotTimerExpired, FireRate);
 			}
-
 		}
 	}
-
 
 }
 
 void APlayerPawn::ShotTimerExpired()
 {
-	bCanFire = true;
+	bCanShoot = true;
 }
 
 void APlayerPawn::PlayerHit()
 {
-	Health -= 20;
+	Health -= DamagePerShot;
 
 	if (Health <= 0)
 	{
-		//todo respawn function
-		Health = 100;
-		SetActorLocationAndRotation(StartingLocation,StartingRotation);
+		SetActorHiddenInGame(true);
+		SetActorEnableCollision(false);
+		GetWorld()->GetTimerManager().SetTimer(TimerHandle_RespawnTimerExpired, this, &APlayerPawn::Respawn, RespawnRate);
 	}
 
+}
+
+void APlayerPawn::Respawn()
+{
+	Health = FullHealth;
+	
+	SetActorHiddenInGame(false);
+	SetActorEnableCollision(true);	
+	SetActorLocationAndRotation(StartingLocation, StartingRotation);	
 }
 
 // Called to bind functionality to input
@@ -152,8 +153,8 @@ void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	PlayerInputComponent->BindAction("Grow", IE_Pressed, this, &APlayerPawn::StartGrowing);
-	PlayerInputComponent->BindAction("Grow", IE_Released, this, &APlayerPawn::StopGrowing);
+	PlayerInputComponent->BindAction("Shoot", IE_Pressed, this, &APlayerPawn::StartShooting);
+	PlayerInputComponent->BindAction("Shoot", IE_Released, this, &APlayerPawn::StopShooting);
 
 	PlayerInputComponent->BindAxis("MoveX", this, &APlayerPawn::Move_XAxis);
 	PlayerInputComponent->BindAxis("MoveY", this, &APlayerPawn::Move_YAxis);
@@ -165,37 +166,32 @@ void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 
 void APlayerPawn::Move_XAxis(float AxisValue)
 {
-	// Move at 100 units per second forward or backward
-	CurrentVelocity.X = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 500.0f;
+	MovementDirection.X = FMath::Clamp(AxisValue, InputClampMin, InputClampMax);
 }
 
 void APlayerPawn::Move_YAxis(float AxisValue)
 {
-	// Move at 100 units per second right or left
-	CurrentVelocity.Y = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 500.0f;
+	MovementDirection.Y = FMath::Clamp(AxisValue, -InputClampMin, InputClampMax);
 }
 
 void APlayerPawn::Rotate_XAxis(float AxisValue)
-{
-	
-	RotationDirection.X = FMath::Clamp(AxisValue, -1.0f, 1.0f);
-
+{	
+	RotationDirection.X = FMath::Clamp(AxisValue, -InputClampMin, InputClampMax);
 }
 
 void APlayerPawn::Rotate_YAxis(float AxisValue)
 {
-	
-	RotationDirection.Y = FMath::Clamp(AxisValue, -1.0f, 1.0f);
+	RotationDirection.Y = FMath::Clamp(AxisValue, -InputClampMin, InputClampMax);
 }
 
-void APlayerPawn::StartGrowing()
+void APlayerPawn::StartShooting()
 {
-	bGrowing = true;		
+	bShooting = true;		
 }
 
-void APlayerPawn::StopGrowing()
+void APlayerPawn::StopShooting()
 {
-	bGrowing = false;
+	bShooting = false;
 }
 
 UPawnMovementComponent* APlayerPawn::GetMovementComponent() const
--- Source/Hologate/PlayerPawn.h
+++ Source/Hologate/PlayerPawn.h
@@ -27,54 +27,56 @@ public:
 		TSubclassOf<class AProjectile> Projectile;
 
 	/* How fast the weapon will fire */
-	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
+	UPROPERTY(Category = Gameplay, EditDefaultsOnly, BlueprintReadWrite)
 		float FireRate;
 
-
+	UPROPERTY(Category = Gameplay, EditDefaultsOnly, BlueprintReadWrite)
+		float RespawnRate;
+	
+	void PlayerHit();
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	//Input functions
-	void Move_XAxis(float AxisValue);
-	void Move_YAxis(float AxisValue);
-	void Rotate_XAxis(float AxisValue);
-	void Rotate_YAxis(float AxisValue);
-	void StartGrowing();
-	void StopGrowing();
+				
+	void ShotTimerExpired();
+	void Respawn();
 
 	virtual UPawnMovementComponent* GetMovementComponent() const override;
 
-	//Input variables
-	FVector CurrentVelocity;
-	FVector RotationDirection;
-	bool bGrowing;
-
-	/* Handler for the fire timer expiry */
-	void ShotTimerExpired();
-
-	void PlayerHit();
-
 	class UPlayerMovementComponent* OurMovementComponent;
-
+	
 private :
 
-	/* Flag to control firing  */
-	bool bCanFire;
+	//Flag to control when a shot can be fired (Fire Rate)  
+	bool bCanShoot;
 
-	/** Handle for efficient management of ShotTimerExpired timer */
+	//Handle for timer expiries
 	FTimerHandle TimerHandle_ShotTimerExpired;
+	FTimerHandle TimerHandle_RespawnTimerExpired;
 
 	int32 Health;
 
 	FVector StartingLocation;
 	FRotator StartingRotation;
 
+	//Input variables
+	FVector MovementDirection;
+	FVector RotationDirection;
+	bool bShooting;
+	
+	//Input functions
+	void Move_XAxis(float AxisValue);
+	void Move_YAxis(float AxisValue);
+	void Rotate_XAxis(float AxisValue);
+	void Rotate_YAxis(float AxisValue);
+	void StartShooting();
+	void StopShooting();
+	   	 
 };
